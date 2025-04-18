19f202f3ce4e17312a9359652c9d38eccb770b17
DANIELGDM180 <108919916+davismasondaniel@users.noreply.github.com>
2025-04-18 22:08:13 +0100

    Update client.lua

--- client.lua
+++ client.lua
@@ -169,7 +169,6 @@ CreateThread(function()
         MpSecurityGarage.LoadDefault() -- -1071.4387, -77.033875, -93.525505
         MpSecurityMusicRoofTop.LoadDefault() -- -592.6896, 273.1052, 116.302444
         MpSecurityStudio.LoadDefault() -- -1000.7252, -70.559875, -98.10669
-        MpSecurityBillboards.LoadDefault() -- -592.6896, 273.1052, 116.302444
         MpSecurityOffice1.LoadDefault() -- -1021.86084, -427.74564, 68.95764
         MpSecurityOffice2.LoadDefault() -- 383.4156, -59.878227, 108.4595
         MpSecurityOffice3.LoadDefault() -- -1004.23035, -761.2084, 66.99069
