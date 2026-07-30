590744f3bd204c30e32c843b78154b6527943f54
davismasondaniel <108919916+davismasondaniel@users.noreply.github.com>
2026-07-30 18:06:47 +0100

    Add Kortz Center Heist DLC support
    Add support for "The Kortz Center Heist" (mp2026_01): new dlc_kortz modules (base, museum, exhibition, basement, loadingbay, mansion_workshop, sewers) and register them in fxmanifest. Replace build-number guards with IsDlcPresent checks for Money Fronts and A Safehouse in the Hills. Bump version to 2.7.0 and update README (title and changelog). Update various stream asset files (binary asset updates).

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,7 +3,7 @@ game 'gta5'
 
 author 'Bob_74'
 description 'Load and customize your map'
-version '2.6.1'
+version '2.7.0'
 
 lua54 "yes"
 
@@ -179,4 +179,13 @@ client_scripts {
     , "dlc_mansions/mansion_basement1.lua"
     , "dlc_mansions/mansion_basement2.lua"
     , "dlc_mansions/mansion_basement3.lua"
+
+    -- DLC The Kortz Center Heist (Requires forced build 3889 or higher)
+    , "dlc_kortz/base.lua"
+    , "dlc_kortz/museum.lua"
+    , "dlc_kortz/exhibition.lua"
+    , "dlc_kortz/basement.lua"
+    , "dlc_kortz/loadingbay.lua"
+    , "dlc_kortz/sewers.lua"
+    , "dlc_kortz/mansion_workshop.lua"
 }
